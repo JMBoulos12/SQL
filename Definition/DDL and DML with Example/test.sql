51c4f642404e9f23596bc3b5f37af6482b41d08e
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-20 14:41:21 +0200

    Create test.sql

---
+++ Definition/DDL and DML with Example/test.sql
@@ -0,0 +1,7 @@
+Q :  What are DDL and DML? Give one command of each
+
+A : DDL stands for Data Definition Language. DDL commands are used to manipulate the
+    database objects like database, table, views etc. In simple words DDL commands are used to
+    create table, changing the structure of table or dropping the table. Example: CREATE, ALTER & DROP
+    DML stands for Data Manipulation Language. DML commands are used to manipulate the
+    information stored in a table. Like adding new records,
