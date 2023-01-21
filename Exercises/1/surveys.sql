53b3c66fd6916e162470ee055914d33ca4790c3e
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-21 18:46:27 +0200

    Create surveys.sql

---
+++ Exercises/1/surveys.sql
@@ -0,0 +1,13 @@
+
+
+
+
+
+/*
+  Write a query that returns the year, month, day, species ID and weight (in mg). 
+  21-January-2023
+*/
+
+
+SELECT day, month, year, species_id, weight * 1000
+FROM surveys;
